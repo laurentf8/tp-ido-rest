27435aa74697d97f19d3d34dadef6b0713b004cb
ferrin <ferrin@202-18>
2020-11-24 17:51:55 +0100

    test de config serv rest

---
+++ test.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+print "Content-Type: text/html\n\n";
+print ("<h1>Perl is working!</h1>");
+
+print ("<h2>Laurent</h2>");
+
+print ("<button>");
+print ("allume");
+print ("</button>");
+
+
